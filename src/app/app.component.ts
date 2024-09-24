e6b5a0d9a04116ad8308571ea074f3855bc8d18e
Galina Antipin <galina.antipin@gmail.com>
2024-09-24 13:07:22 +0200

    added fonts folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { AboutMeComponent } from './main-content/about-me/about-me.component';
+import { ContactComponent } from './main-content/contact/contact.component';
+import { HeroComponent } from './main-content/hero/hero.component';
+import { PortfolioComponent } from './main-content/portfolio/portfolio.component';
+import { ReferencesComponent } from './main-content/references/references.component';
+import { SkillsComponent } from './main-content/skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainContentComponent, FooterComponent, ImprintComponent, PrivacyPolicyComponent, HeaderComponent, AboutMeComponent, ContactComponent, HeroComponent, PortfolioComponent, ReferencesComponent, SkillsComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
