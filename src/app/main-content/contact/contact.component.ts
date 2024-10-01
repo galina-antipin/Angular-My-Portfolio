d29837c5303bd6e74d3985152f54f15b68806796
Galina Antipin <galina.antipin@gmail.com>
2024-10-01 17:17:42 +0200

    added footer html

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-contact',
@@ -10,7 +11,17 @@ import { FormsModule, NgForm } from '@angular/forms';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+  constructor() { }
+
+  ngOnInit() {
+    AOS.init({
+      duration: 1200,
+      easing: 'ease-in-out', 
+      once: true, 
+      mirror: false,
+    });
+  }
 
   http = inject(HttpClient);
 
@@ -18,6 +29,7 @@ export class ContactComponent {
     name: '',
     email: '',
     message: '',
+    acceptPolicy: false,
   }
 
   mailTest = true;
@@ -46,8 +58,12 @@ export class ContactComponent {
           },
           complete: () => console.info('send post complete'),
         });
+      }
+        else if (!this.contactData.acceptPolicy) {
+          console.error('privacy policy not accepted');
     } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
 
       ngForm.resetForm();
+      this.contactData.acceptPolicy = false;
     }
   }}
