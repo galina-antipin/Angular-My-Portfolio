9864b0d2891dc62c660da0cc74cfc0aa9a028ef5
Galina Antipin <galina.antipin@gmail.com>
2024-10-01 08:45:58 +0200

    added aos library on portfolio component

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
+  constructor() { }
 
-}
+    ngOnInit() {
+      AOS.init({
+        duration: 1200,
+        easing: 'ease-in-out', 
+        once: true, 
+        mirror: false,
+      });
+    }
+  
+  }
+
