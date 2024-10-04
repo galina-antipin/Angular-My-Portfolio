67ac7ed85309a8ebcc3d485e50d044b979e0e514
Galina Antipin <galina.antipin@gmail.com>
2024-10-04 11:31:13 +0200

    fixed imprint section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainContentComponent},
+    {path: 'imprint', component: ImprintComponent},
+
+];
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
+import { ImprintComponent } from '../imprint/imprint.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ReferencesComponent, ContactComponent, ImprintComponent, RouterOutlet],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
